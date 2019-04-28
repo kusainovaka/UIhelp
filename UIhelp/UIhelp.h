09eb83188788116288883c776e5e24a2f5f97a4d
Kamila <kusainovaka@gmail.com>
2019-04-29 00:47:47 +0600

    Initial project setup

---
+++ UIhelp/UIhelp.h
@@ -0,0 +1,19 @@
+//
+//  UIhelp.h
+//  UIhelp
+//
+//  Created by Kamila on 4/29/19.
+//  Copyright © 2019 Kamila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIhelp.
+FOUNDATION_EXPORT double UIhelpVersionNumber;
+
+//! Project version string for UIhelp.
+FOUNDATION_EXPORT const unsigned char UIhelpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIhelp/PublicHeader.h>
+
+
